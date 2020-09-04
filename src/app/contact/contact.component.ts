8936883c17bd98d09130dea151d1b6c0b55ec708
itskunal260316 <tkunal23@gmail.com>
2020-09-05 01:02:37 +0530

    sent user message to db from contact us

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormControl, FormControlName, FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +10,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private http: HttpClient, private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+  isMessageSent = false;
+  public isContactEmailValid = false;
+  public isContactPhone = false;
+
+  public contactMsg = this.fb.group({
+
+    name: [''],
+    email: ['', Validators.required],
+    phone: [''],
+    msg: ['', Validators.required]
+
+  });
+
+  userMsg = {};
+
+  async saveMsg() {
+    this.userMsg = this.contactMsg.value;
+    console.log(this.userMsg);
+    const url = 'http://localhost:5700/saveMsg';
+    let res = await this.http.post(url, this.userMsg).toPromise();
+
+    if (res != 0) {
+      this.isMessageSent = true;
+    }
+    else {
+      this.isMessageSent = false;
+    }
+
+    this.contactMsg.setValue({
+
+      name: [''],
+      email: [''],
+      phone: [''],
+      msg: ['']
+
+    });
+
+  }
+
+
+
+
+  validateContactEmail() {
+
+    this.userMsg = this.contactMsg.value;
+    let userEmail = this.contactMsg.value.email;
+    let regex = /^([a-zA_Z0-9\.@_]+)@([a-zA_Z0-9\.-_]{2,20}).([a-z]{2,3})(\.[a-z]{2,10})$/
+    if (regex.test(userEmail)) {
+      console.log(userEmail);
+      this.isContactEmailValid = false;
+    }
+    else {
+      this.isContactEmailValid = true;
+    }
+
+
+  }
+
+
+
+
 
 }
